4bcc03107832ca51c0a149a7ec4bc7a4f8fb50a4
communal-documentation-0347 <scottkoons@gmail.com>
2020-07-28 16:13:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
+// function destructivelyAppendKitten(kittens) {
+//     kittens.push('Ralph')
+//     return kittens;
+// }
+// destructivelyAppendKitten(kittens);
+function destructivelyAppendKitten(kittenNames){
+    return kittens.push(kittenNames);
+}
+
+function destructivelyPrependKitten(kittenNames){
+    return kittens.unshift(kittenNames);
+}
+
+function destructivelyRemoveLastKitten(kittenNames){
+    return kittens.pop(kittenNames);
+}
+
+function destructivelyRemoveFirstKitten(kittenNames){
+    return kittens.shift(kittenNames);
+}
+
+function appendKitten(kittenNames){
+    return kittens.appendKitten(kittenNames);
+}
